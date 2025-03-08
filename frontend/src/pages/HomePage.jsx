79978a4111a66f7ead1f389ad2277ae2185e1420
Himanshu <himanshukumar182003@gmail.com>
2025-03-09 00:44:23 +0530

    Removed Carousel from Home Page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
-import Carousel from '../components/Carousel/Carousel';
 import Footer from '../components/Footer/Footer';
 
 function HomePage() {
   return (
     <>
     <Navbar />
-    <Carousel />
     <Footer />
     </>
   );
